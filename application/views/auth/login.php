3001e6d5d64e92867c7c787623ccef29de0f3e38
nabillanurfadillah <nurfadillahnabilla@gmail.com>
2020-03-22 15:35:21 +0700

    perubahan tampilan halaman login

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -3,16 +3,16 @@
     <!-- Outer Row -->
     <div class="row justify-content-center">
 
-        <div class="col-lg-7">
+        <div class="col-lg-5">
 
-            <div class="card o-hidden border-0 shadow-lg my-5">
+            <div class="card my-5">
                 <div class="card-body p-0">
                     <!-- Nested Row within Card Body -->
                     <div class="row">
                         <div class="col-lg">
                             <div class="p-5">
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
+                                    <h1 class="h4 text-gray-900 mb-5">Login Page</h1>
                                 </div>
                                 <?= $this->session->flashdata('message'); ?>
                                 <form class="user" method="post" action="?>= base_url('auth'); ?>">
@@ -25,7 +25,7 @@
                                         <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
                                     </div>
 
-                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                    <button type="submit" class="btn btn-o btn-user btn-block">
                                         Login
                                     </button>
                                     <hr>
--- application/views/templates/auth_header.php
+++ application/views/templates/auth_header.php
@@ -11,13 +11,16 @@
 
     <title><?= $title; ?></title>
 
-    <!-- Custom fonts for this template-->
-    <link href="<?= base_url('assets/'); ?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v5.12.1/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+    <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
 
     <!-- Custom styles for this template-->
     <link href="<?= base_url('assets/'); ?>css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="<?= base_url('assets/'); ?>css/login.css" rel="stylesheet">
 
 </head>
 
-<body class="bg-gradient-primary">
+<body>

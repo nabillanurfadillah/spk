7a18867d0056e04ff3a1b3e31577476acf650e5e
nabillanurfadillah <nurfadillahnabilla@gmail.com>
2020-05-16 11:40:47 +0700

    merapikan footer

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -21,12 +21,10 @@
     <div class="row">
         <div class="col-lg-4">
             <h4 class="text-center">Nilai Preferensi</h4>
-
             <div class="card">
                 <div class="card-body">
                     <ol>
                         <?php foreach ($nilai as $n) : ?>
-
                             <li><?= $n['keterangan_nilai']; ?></li>
                         <?php endforeach; ?>
                     </ol>
@@ -35,12 +33,10 @@
         </div>
         <div class="col-lg-4">
             <h4 class="text-center">Kriteria-Kriteria</h4>
-
             <div class="card">
                 <div class="card-body">
                     <ol>
                         <?php foreach ($kriteria as $k) : ?>
-
                             <li><?= $k['nama_kriteria']; ?></li>
                         <?php endforeach; ?>
                     </ol>
@@ -49,12 +45,10 @@
         </div>
         <div class="col-lg-4">
             <h4 class="text-center">Alternatif</h4>
-
             <div class="card">
                 <div class="card-body">
                     <ol>
                         <?php foreach ($alternatif as $a) : ?>
-
                             <li><?= $a['nama_alternatif']; ?></li>
                         <?php endforeach; ?>
                     </ol>
@@ -66,38 +60,38 @@
     <div class="container2">
         <canvas id="myChart"></canvas>
     </div>
-
-    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
-    <script type="text/javascript">
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var chart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: [
+</div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+<script type="text/javascript">
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [
+                <?php
+                if (count($graph) > 0) {
+                    foreach ($graph as $data) {
+                        echo "'" . $data->nama_alternatif . "',";
+                    }
+                }
+                ?>
+            ],
+            datasets: [{
+                label: 'Alternatif',
+                backgroundColor: '#ADD8E6',
+                borderColor: '##93C3D2',
+                data: [
                     <?php
                     if (count($graph) > 0) {
                         foreach ($graph as $data) {
-                            echo "'" . $data->nama_alternatif . "',";
+                            echo $data->hasil . ", ";
                         }
                     }
                     ?>
-                ],
-                datasets: [{
-                    label: 'Alternatif',
-                    backgroundColor: '#ADD8E6',
-                    borderColor: '##93C3D2',
-                    data: [
-                        <?php
-                        if (count($graph) > 0) {
-                            foreach ($graph as $data) {
-                                echo $data->hasil . ", ";
-                            }
-                        }
-                        ?>
-                    ]
-                }]
-            },
-        });
-    </script>
-</div>
+                ]
+            }]
+        },
+    });
+</script>
 <!-- End of Main Content -->
